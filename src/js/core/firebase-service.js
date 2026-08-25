90e0c29289359274ac43f9240aeb218bbd4f5084
osartori <o.sartori@gmail.com>
2026-08-25 12:06:19 +0200

    debug

--- src/js/core/firebase-service.js
+++ src/js/core/firebase-service.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getDatabase, ref, onValue, push, set, update, remove } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
-import { FIREBASE_CONFIG, DB_PATHS } from "../config/firebase-config.js";
+import { FIREBASE_CONFIG, DB_PATHS } from "../../config/firebase-config.js";
 
 const app = initializeApp(FIREBASE_CONFIG);
 export const db = getDatabase(app);
