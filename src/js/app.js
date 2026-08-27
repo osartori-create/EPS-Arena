a3a1a83d3042a2c76fae19854a286b34ddff23d5
osartori <o.sartori@gmail.com>
2026-08-27 13:17:48 +0200

    debug

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import { initLayout } from './ui/prof/layout.js';
+import { initLiveUI } from './ui/prof/live.js';
 
 export function initApp() {
-    console.log("TEST : initLayout uniquement");
+    console.log("TEST : initLayout + initLiveUI");
     initLayout();
+    initLiveUI();
 }
