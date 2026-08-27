af64e542c769221760d28b29d6fc5bfdf5467b47
osartori <o.sartori@gmail.com>
2026-08-27 10:44:22 +0200

    debug

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,7 @@
 // src/js/app.js
-import { listenConfig } from './core/firebase-service.js';
-import { updateState } from './core/state.js';
 import { initLayout } from './ui/prof/layout.js';
-import { initActivities } from './ui/prof/activities.js';
-import { initAdminUI } from './ui/dashboard-ui.js';
-import { initLiveUI } from './ui/prof/live.js'; // Le Live gère maintenant la TV
 
 export function initApp() {
-    console.log("✅ EPS-Arena démarré !");
+    console.log("⚠️ TEST : initLayout uniquement");
     initLayout();
-    initAdminUI();
-    initActivities();
-    initLiveUI(); // Cette fonction gère le Live et l'onglet TV (via live.js)
-    
-    listenConfig((config) => {
-        updateState('equipesConfig', config.equipes || {});
-    });
 }
