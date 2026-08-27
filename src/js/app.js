e20554db6646b7bebf58d78c0edfe3ab2962f8a0
osartori <o.sartori@gmail.com>
2026-08-27 10:45:34 +0200

    debug

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 // src/js/app.js
 import { initLayout } from './ui/prof/layout.js';
+import { initAdminUI } from './ui/dashboard-ui.js';
 
 export function initApp() {
-    console.log("⚠️ TEST : initLayout uniquement");
+    console.log("⚠️ TEST : initLayout + initAdminUI");
     initLayout();
+    initAdminUI();
 }
