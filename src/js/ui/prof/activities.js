040e6026420a3977ac8ce765c8f4f4f0554ef658
osartori <o.sartori@gmail.com>
2026-08-29 12:12:00 +0200

    debug

--- src/js/ui/prof/activities.js
+++ src/js/ui/prof/activities.js
@@ -11,7 +11,7 @@ import {
     exportOrientShowConfig,
     importOrientShowConfig,
     startOrientShow,
-    stopOrientShow
+    stopOrientShow,
     getMatrix
 } from '../../modules/orientshow/orientshow-interface.js';
 
