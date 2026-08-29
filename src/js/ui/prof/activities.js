3eb28ae31045431b8e77caa86310a07e2df8081f
osartori <o.sartori@gmail.com>
2026-08-29 12:21:57 +0200

    debug

--- src/js/ui/prof/activities.js
+++ src/js/ui/prof/activities.js
@@ -11,8 +11,7 @@ import {
     exportOrientShowConfig,
     importOrientShowConfig,
     startOrientShow,
-    stopOrientShow,
-    getMatrix
+    stopOrientShow
 } from '../../modules/orientshow/orientshow-interface.js';
 
 let currentDiscipline = 'multi';
@@ -165,11 +164,22 @@ export function initActivities() {
             }
         });
     } 
-    else if (currentDiscipline === 'orientshow') {
-    // Importer la fonction getMatrix depuis orientshow-interface
-    // (en haut du fichier, ajouter : import { getMatrix } from '../../modules/orientshow/orientshow-interface.js';)
-    const matrix = getMatrix(); // récupère la matrice (avec fallback par défaut)
-    
+     // Codes par défaut pour la matrice (intégrés en dur)
+    const DEFAULT_OS_MATRIX = {
+        1: { NOIR: ['D','Q'], ROUGE: ['O','U'], BLEU: ['Y','A'], VERT: ['E','R'], JAUNE: ['N','K'] },
+        2: { NOIR: ['E','X'], ROUGE: ['X','Y'], BLEU: ['T','L'], VERT: ['R','O'], JAUNE: ['A','L'] },
+        3: { NOIR: ['C','L'], ROUGE: ['H','U'], BLEU: ['I','B'], VERT: ['O','I'], JAUNE: ['T','E'] },
+        4: { NOIR: ['R','V'], ROUGE: ['E','E'], BLEU: ['C','R'], VERT: ['T','N'], JAUNE: ['O','I'] },
+        5: { NOIR: ['A','B'], ROUGE: ['J','O'], BLEU: ['O','U'], VERT: ['N','E'], JAUNE: ['C','S'] },
+        6: { NOIR: ['F','M'], ROUGE: ['I','E'], BLEU: ['C','R'], VERT: ['U','O'], JAUNE: ['S','U'] },
+        7: { NOIR: ['G','H'], ROUGE: ['U','A'], BLEU: ['E','C'], VERT: ['U','H'], JAUNE: ['X','E'] },
+        8: { NOIR: ['I','J'], ROUGE: ['V','E'], BLEU: ['R','A'], VERT: ['E','N'], JAUNE: ['S','S'] },
+        9: { NOIR: ['K','N'], ROUGE: ['R','Y'], BLEU: ['A','L'], VERT: ['F','O'], JAUNE: ['T','N'] },
+        10: { NOIR: ['O','S'], ROUGE: ['C','E'], BLEU: ['E','I'], VERT: ['A','Z'], JAUNE: ['N','E'] },
+        11: { NOIR: ['P','T'], ROUGE: ['A','U'], BLEU: ['L','Y'], VERT: ['U','A'], JAUNE: ['D','U'] },
+        12: { NOIR: ['U','W'], ROUGE: ['L','I'], BLEU: ['T','N'], VERT: ['R','C'], JAUNE: ['A','H'] }
+    };
+
     const orientShowMapping = JSON.parse(localStorage.getItem(`eps_arena_local_mapping_${activeClasse}`) || '{}');
     const codeCounts = {};
     Object.keys(orientShowMapping).forEach(key => {
@@ -187,10 +197,10 @@ export function initActivities() {
         configData[couleur] = codeCounts[couleur];
     });
     
-    // Utiliser la matrice récupérée
-    configData.matrix = matrix;
+    // Utiliser la matrice en dur (garantie complète)
+    configData.matrix = DEFAULT_OS_MATRIX;
     
-    // Récupération des temps
+    // Récupération des temps (version robuste)
     const startTimeStr = localStorage.getItem('eps_arena_os_startTime');
     const endTimeStr = localStorage.getItem('eps_arena_os_endTime');
     const parseTime = (value) => {
